a2ba4b9ad9bab2de43d9193bc48eca7120eb3138
Radi Atanassov <radi@sharepoint.bg>
2023-09-17 07:48:24 +0300

    small updates

--- src/AzureFormRecognizerDemo/AzureFormRecognizerDemo/Program.cs
+++ src/AzureFormRecognizerDemo/AzureFormRecognizerDemo/Program.cs
@@ -4,29 +4,52 @@ using System.Threading.Tasks;
 
 string key = "";
 string endpoint = "https://globalazureforms.cognitiveservices.azure.com/";
-string modelId = "test-model-5";
 
 var credential = new AzureKeyCredential(key);
 var client = new DocumentAnalysisClient(new Uri(endpoint), credential);
 
-foreach (var file in Directory.GetFiles("testdata"))
-{
-    Console.WriteLine($"Analyzing: \\{file}");
-    using var stream = File.OpenRead(file);
+await GetClassification(modelId: "test-model-5", client);
 
-    // If using a pre-built model:
-    //var result = await client.AnalyzeDocumentAsync(WaitUntil.Completed, modelId, stream);
-
-    // If using a classifier:
-    var result = await client.ClassifyDocumentAsync(WaitUntil.Completed, modelId, stream);
-
-    if (result.HasValue && result.Value.Documents.Any())
-    {
-        var documentType = result.Value.Documents.First().DocumentType;
-
-        Console.WriteLine($"The document type is: {documentType}");
-    }
-}
+await GetContents(modelId: "prebuilt-read", client);
 
 Console.WriteLine("Done.");
 Console.ReadLine();
+
+static async Task GetClassification(string modelId, DocumentAnalysisClient client)
+{
+    foreach (var file in Directory.GetFiles("classification-test-data"))
+    {
+        Console.WriteLine($"Analyzing: \\{file}");
+        using var stream = File.OpenRead(file);
+
+        // If using a classifier:
+        var result = await client.ClassifyDocumentAsync(WaitUntil.Completed, modelId, stream);
+
+        if (!result.HasValue) continue;
+
+        if (result.Value.Documents.Any())
+        {
+            var documentType = result.Value.Documents.First().DocumentType;
+
+            Console.WriteLine($"The document type is: {documentType}");
+        }
+    }
+}
+
+static async Task GetContents(string modelId, DocumentAnalysisClient client)
+{
+    foreach (var file in Directory.GetFiles("extraction-test-data"))
+    {
+        Console.WriteLine($"Analyzing: \\{file}");
+        using var stream = File.OpenRead(file);
+
+        // If using a analyze:
+        var result = await client.AnalyzeDocumentAsync(WaitUntil.Completed, modelId, stream);
+
+        if (!result.HasValue) continue;
+
+        var documentContent = result.Value.Content;
+
+        Console.WriteLine($"The document content is: {documentContent}");
+    }
+}
